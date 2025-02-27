cce4912c75c3c3e3f99ed956d2f833a0e73df06d
vishwakumara <chamika@3cs.solutions>
2025-02-27 11:30:31 +0530

    changes

--- backend/database.js
+++ backend/database.js
@@ -1,55 +1,58 @@
 const knex_db = require("./db-config");
 
+// Teacher functions
 const readTeachers = async () => {
     const sql = `SELECT * FROM teacher`;
     return knex_db.raw(sql);
-}
+};
 
 const readTeacherInfo = async (id) => {
     const sql = `SELECT * FROM teacher WHERE id = ?`;
     return knex_db.raw(sql, [id]);
-}
+};
 
 const addTeacher = async (id, name, age) => {
     const sql = `INSERT INTO teacher(id, name, age) VALUES (?, ?, ?)`;
     return knex_db.raw(sql, [id, name, age]);
-}
+};
 
 const updateTeacher = async (name, age, id) => {
     const sql = `UPDATE teacher SET name=?, age=? WHERE id=?`;
     return knex_db.raw(sql, [name, age, id]);
-}
+};
 
 const deleteTeacher = async (id) => {
     const sql = `DELETE FROM teacher WHERE id = ?`;
     return knex_db.raw(sql, [id]);
-}
+};
 
+// Student functions (Updated as per requirements)
 const readStudents = async () => {
     const sql = `SELECT * FROM student`;
     return knex_db.raw(sql);
-}
+};
 
 const readStudentInfo = async (id) => {
     const sql = `SELECT * FROM student WHERE id = ?`;
     return knex_db.raw(sql, [id]);
-}
+};
 
 const addStudent = async (id, name, age, religion) => {
     const sql = `INSERT INTO student(id, name, age, religion) VALUES (?, ?, ?, ?)`;
     return knex_db.raw(sql, [id, name, age, religion]);
-}
+};
 
 const updateStudent = async (name, age, religion, id) => {
     const sql = `UPDATE student SET name=?, age=?, religion=? WHERE id=?`;
     return knex_db.raw(sql, [name, age, religion, id]);
-}
+};
 
 const deleteStudent = async (id) => {
     const sql = `DELETE FROM student WHERE id = ?`;
     return knex_db.raw(sql, [id]);
-}
+};
 
+// Exporting all functions
 module.exports = {
     readTeachers,
     readStudents,
